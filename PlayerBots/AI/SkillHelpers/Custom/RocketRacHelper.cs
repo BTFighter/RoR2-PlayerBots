39fc49a6fd115b67801a5ecafb1d7bbdfa223274
BTFighter <45516471+BTFighter@users.noreply.github.com>
2025-05-03 22:01:40 +0800

    Huntress fix...
    ...and adding Rocket Compatability. Huntress' file was left blank, oops. Also, Donniecoe828's changes weren't removed, forgot his fork was using a different name.

--- PlayerBots/AI/SkillHelpers/Custom/RocketRacHelper.cs
+++ PlayerBots/AI/SkillHelpers/Custom/RocketRacHelper.cs
@@ -1,21 +1,57 @@
-﻿﻿using RoR2;
-using RoR2.CharacterAI;
+﻿using RoR2.CharacterAI;
 using UnityEngine;
 
-namespace PlayerBots.AI.SkillHelpers
+namespace PlayerBots.AI.SkillHelpers.Custom
 {
-    [SkillHelperSurvivor("HuntressBody")]
-    class HuntressHelper : AiSkillHelper
+    [SkillHelperSurvivor("RocketRacBody")]
+    [CustomSurvivor("https://thunderstore.io/package/VCR/RocketRaccoon/", "1.0.4")]
+    class RocketRacHelper : AiSkillHelper
     {
         public override void InjectSkills(GameObject gameObject, BaseAI ai)
         {
+            AISkillDriver skill2 = gameObject.AddComponent<AISkillDriver>() as AISkillDriver;
+            skill2.customName = "Secondary";
+            skill2.skillSlot = RoR2.SkillSlot.Secondary;
+            skill2.requireSkillReady = true;
+            skill2.moveTargetType = AISkillDriver.TargetType.CurrentEnemy;
+            skill2.minDistance = 0;
+            skill2.maxDistance = 15;
+            skill2.selectionRequiresTargetLoS = true;
+            skill2.activationRequiresTargetLoS = true;
+            skill2.activationRequiresAimConfirmation = false;
+            skill2.movementType = AISkillDriver.MovementType.FleeMoveTarget;
+            skill2.aimType = AISkillDriver.AimType.AtCurrentEnemy;
+            skill2.ignoreNodeGraph = false;
+            skill2.resetCurrentEnemyOnNextDriverSelection = true;
+            skill2.noRepeat = true;
+            skill2.shouldSprint = true;
+
+
+            AISkillDriver skill3_chase = gameObject.AddComponent<AISkillDriver>() as AISkillDriver;
+            skill3_chase.customName = "UtilityChase";
+            skill3_chase.skillSlot = RoR2.SkillSlot.Utility;
+            skill3_chase.requireSkillReady = true;
+            skill3_chase.moveTargetType = AISkillDriver.TargetType.CurrentEnemy;
+            skill3_chase.minDistance = 50;
+            skill3_chase.maxDistance = 100;
+            skill3_chase.selectionRequiresTargetLoS = true;
+            skill3_chase.activationRequiresTargetLoS = true;
+            skill3_chase.activationRequiresAimConfirmation = false;
+            skill3_chase.movementType = AISkillDriver.MovementType.ChaseMoveTarget;
+            skill3_chase.aimType = AISkillDriver.AimType.MoveDirection;
+            skill3_chase.ignoreNodeGraph = false;
+            skill3_chase.resetCurrentEnemyOnNextDriverSelection = false;
+            skill3_chase.noRepeat = false;
+            skill3_chase.shouldSprint = true;
+
             AISkillDriver skill3 = gameObject.AddComponent<AISkillDriver>() as AISkillDriver;
-            skill3.customName = "Utility";
+            skill3.customName = "UtilityDefensive";
             skill3.skillSlot = RoR2.SkillSlot.Utility;
             skill3.requireSkillReady = true;
             skill3.moveTargetType = AISkillDriver.TargetType.CurrentEnemy;
             skill3.minDistance = 0;
             skill3.maxDistance = 20;
+            //skill3.maxUserHealthFraction = .25f;
             skill3.selectionRequiresTargetLoS = true;
             skill3.activationRequiresTargetLoS = false;
             skill3.activationRequiresAimConfirmation = false;
@@ -25,6 +61,7 @@ namespace PlayerBots.AI.SkillHelpers
             skill3.resetCurrentEnemyOnNextDriverSelection = false;
             skill3.noRepeat = false;
             skill3.shouldSprint = true;
+            skill3.buttonPressType = AISkillDriver.ButtonPressType.TapContinuous;
 
             AISkillDriver skill4 = gameObject.AddComponent<AISkillDriver>() as AISkillDriver;
             skill4.customName = "Special";
@@ -44,28 +81,9 @@ namespace PlayerBots.AI.SkillHelpers
             skill4.shouldSprint = false;
             skill4.buttonPressType = AISkillDriver.ButtonPressType.TapContinuous;
 
-
-
-            AISkillDriver skill2 = gameObject.AddComponent<AISkillDriver>() as AISkillDriver;
-            skill2.customName = "Secondary";
-            skill2.skillSlot = RoR2.SkillSlot.Secondary;
-            skill2.requireSkillReady = true;
-            skill2.moveTargetType = AISkillDriver.TargetType.CurrentEnemy;
-            skill2.minDistance = 0;
-            skill2.maxDistance = 50;
-            skill2.selectionRequiresTargetLoS = true;
-            skill2.activationRequiresTargetLoS = true;
-            skill2.activationRequiresAimConfirmation = true;
-            skill2.movementType = AISkillDriver.MovementType.StrafeMovetarget;
-            skill2.aimType = AISkillDriver.AimType.AtCurrentEnemy;
-            skill2.ignoreNodeGraph = false;
-            skill2.resetCurrentEnemyOnNextDriverSelection = false;
-            skill2.noRepeat = false;
-            skill2.shouldSprint = false;
-
             // Skills
             AISkillDriver skill1 = gameObject.AddComponent<AISkillDriver>() as AISkillDriver;
-            skill1.customName = "Primary";
+            skill1.customName = "Shoot";
             skill1.skillSlot = RoR2.SkillSlot.Primary;
             skill1.requireSkillReady = true;
             skill1.moveTargetType = AISkillDriver.TargetType.CurrentEnemy;
@@ -75,13 +93,13 @@ namespace PlayerBots.AI.SkillHelpers
             skill1.activationRequiresTargetLoS = true;
             skill1.activationRequiresAimConfirmation = true;
             skill1.movementType = AISkillDriver.MovementType.StrafeMovetarget;
-            skill1.aimType = AISkillDriver.AimType.AtCurrentEnemy;
+            skill1.aimType = AISkillDriver.AimType.AtMoveTarget;
             skill1.ignoreNodeGraph = false;
             skill1.resetCurrentEnemyOnNextDriverSelection = false;
             skill1.noRepeat = false;
-            skill1.shouldSprint = true;
+            skill1.shouldSprint = false;
+            skill1.buttonPressType = AISkillDriver.ButtonPressType.TapContinuous;
 
-            skill4.nextHighPriorityOverride = skill1;
 
             // Add default skills
             AddDefaultSkills(gameObject, ai, 20);
